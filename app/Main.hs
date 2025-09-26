6b7ccbe720a3cf22543879b89bf835446d26fa92
Siemen Zielke <77232580+hexzeug@users.noreply.github.com>
2025-09-27 01:28:53 +0200

    cleaned up implementation of solving systems of linear inequalities

--- app/Main.hs
+++ app/Main.hs
@@ -1,49 +1,89 @@
-data NormalizedInequality a = NormalizedInequality [a] a
+data Term a =
+    Constant a |
+    LinearTerm [a] a |
+    IsolatedVariable |
+    Max [Term a] |
+    Min [Term a]
     deriving Show
 
-addInequalities :: Num a => NormalizedInequality a -> NormalizedInequality a -> NormalizedInequality a
-addInequalities (NormalizedInequality as1 b1) (NormalizedInequality as2 b2) = NormalizedInequality (zipWith (+) as1 as2) (b1 + b2)
+-- scale :: Num a => a -> Term a -> Term a
+-- scale f (Constant a) = Constant (f * a)
+-- scale f (LinearTerm as a) = LinearTerm (map (*f) as) a
 
-data IsolatedInequality a = LEq [a] a | GEq [a] a | Zero (NormalizedInequality a)
+-- neg :: Num a => Term a -> Term a
+-- neg = scale (-1)
+
+-- add :: Num a => Term a -> Term a -> Term a
+-- add (Constant a) (Constant b) = Constant (a + b)
+-- add (Constant a) (LinearTerm bs b) = LinearTerm bs (a + b)
+-- add (LinearTerm as a) (Constant b) = LinearTerm as (a + b)
+-- add (LinearTerm as a) (LinearTerm bs b)
+--     | length as /= length bs = error "Can only add linear terms with equal amount of coefficients"
+--     | otherwise = LinearTerm (zipWith (+) as bs) (a + b)
+
+-- sub :: Num a => Term a -> Term a -> Term a
+-- sub a b = a `add` neg b
+
+
+data Inequality a = Inequality (Term a) (Term a)
     deriving Show
 
-isolateFirst :: (Fractional a, Ord a) => NormalizedInequality a -> IsolatedInequality a
-isolateFirst (NormalizedInequality (0 : as) b) = Zero (NormalizedInequality as b)
-isolateFirst (NormalizedInequality (a1 : as) b) =
-    if a1 > 0
-        then GEq as' b'
-        else LEq as' b'
+normalize :: Num a => Inequality a -> Inequality a
+normalize (Inequality (LinearTerm as a) (LinearTerm bs b)) =
+    Inequality (LinearTerm (zipWith (-) as bs) 0) (Constant (b - a))
+normalize (Inequality (Constant a) (Constant b)) =
+    Inequality (LinearTerm [] 0) (Constant (b - a))
+normalize (Inequality (LinearTerm as a) (Constant b)) =
+    Inequality (LinearTerm as 0) (Constant (b - a))
+normalize (Inequality (Constant a) (LinearTerm bs b)) =
+    Inequality (LinearTerm (map negate bs) 0) (Constant (b - a))
+
+
+
+isolateFirst :: (Ord a, Fractional a) => Inequality a -> Inequality a
+isolateFirst (Inequality (LinearTerm (a1 : as) 0) (Constant b))
+    | a1 > 0 = Inequality IsolatedVariable (LinearTerm as' b')
+    | a1 == 0 = Inequality (LinearTerm as 0) (Constant b)
+    | a1 < 0 = Inequality (LinearTerm as' b') IsolatedVariable
     where
+        as' = map (negate . (/a1)) as
         b' = b / a1
-        as' = map (negate . flip (/) a1) as
 
-partition :: (Fractional a, Ord a) => [NormalizedInequality a] -> ([NormalizedInequality a], [NormalizedInequality a], [NormalizedInequality a])
-partition [] = ([], [], [])
-partition (x : xs) =
-    case isolateFirst x of
-        GEq as b -> (NormalizedInequality (map negate as) b : gs, zs, ls) 
-        Zero z -> (gs, z : zs, ls)
-        LEq as b -> (gs, zs, NormalizedInequality as (-b) : ls)
+reduce :: [Inequality a] -> [Inequality a]
+reduce is =
+    let
+        (gts, zs, lts) = foldr collect ([], [], []) is
+    in
+        Inequality IsolatedVariable (Min gts) : Inequality (Max lts) IsolatedVariable : zs
     where
-        (gs, zs, ls) = partition xs
+        collect (Inequality IsolatedVariable t) (gts, zs, lts) = (t : gts, zs, lts)
+        collect (Inequality t IsolatedVariable) (gts, zs, lts) = (gts, zs, t : lts)
+        collect i (gts, zs, lts) = (gts, i : zs, lts)
 
-combine :: Num a => ([NormalizedInequality a], [NormalizedInequality a]) -> [NormalizedInequality a]
-combine (gs, ls) =
-    [addInequalities g l | g <- gs, l <- ls]
+solve :: (Ord a, Fractional a) => [Inequality a] -> [Inequality a]
+solve = reduce . map isolateFirst
 
-project :: (Fractional a, Ord a) => [NormalizedInequality a] -> [NormalizedInequality a]
-project xs =
-    let (gs, zs, ls) = partition xs
-    in zs ++ combine (gs, ls)
+eliminate :: [Inequality a] -> [Inequality a]
+eliminate ((Inequality IsolatedVariable gt) : (Inequality lt IsolatedVariable) : is) =
+    Inequality lt gt : is
 
-x :: [NormalizedInequality Double]
+expand :: Num a => [Inequality a] -> [Inequality a]
+expand ((Inequality (Max lts) (Min gts)) : is) =
+    [normalize (Inequality lt gt) | lt <- lts, gt <- gts] ++ is
+
+
+project :: (Ord a, Fractional a) => [Inequality a] -> [Inequality a]
+project = expand . eliminate . solve
+
+
+x :: [Inequality Double]
 x =
     [
-        NormalizedInequality [-1, 0] 0,
-        NormalizedInequality [1, 2] 6,
-        NormalizedInequality [-1, -1] (-2),
-        NormalizedInequality [1, -1] 3,
-        NormalizedInequality [0, -1] 0
+        Inequality (LinearTerm [-1, 0] 0) (Constant 0),
+        Inequality (LinearTerm [1, 2] 0) (Constant 6),
+        Inequality (LinearTerm [-1, -1] 0) (Constant (-2)),
+        Inequality (LinearTerm [1, -1] 0) (Constant 3),
+        Inequality (LinearTerm [0, -1] 0) (Constant 0)
     ]
 
 main :: IO ()
